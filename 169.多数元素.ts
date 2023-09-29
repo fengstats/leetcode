fa31a7d729118a1fec132c1029f153944fc078b0
feng <feng2860984180@163.com>
2023-09-29 23:54:56 +0800

    多数元素 | 水个 commit

---
+++ 169.多数元素.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=typescript
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+// 待会来补，水个 commit，今天白天事有点多
+function majorityElement(nums: number[]): number {
+  return -1
+}
+// @lc code=end
