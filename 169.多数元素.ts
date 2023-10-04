dcec0f8c3b584179a1a3cf6b1ab60acd0a5963e3
feng <feng2860984180@163.com>
2023-10-04 21:08:13 +0800

    多数元素 | hashMap

--- 169.多数元素.ts
+++ 169.多数元素.ts
@@ -5,8 +5,29 @@
  */
 
 // @lc code=start
-// 待会来补，水个 commit，今天白天事有点多
+// 思路：hashMap 记录数组元素出现次数进行比较
 function majorityElement(nums: number[]): number {
-  return -1
+  const map = new Map()
+  // 存储最多的出现次数
+  let maxCount = 1
+  // 存储最多出现次数的元素
+  let ans = nums[0]
+
+  for (let i = 0; i < nums.length; i++) {
+    const key = nums[i]
+    if (map.has(key)) {
+      // 比较出现次数的最大
+      const count = map.get(key) + 1
+      if (count > maxCount) {
+        maxCount = count
+        ans = key
+      }
+      map.set(key, count)
+    } else {
+      map.set(key, 1)
+    }
+  }
+
+  return ans
 }
 // @lc code=end
