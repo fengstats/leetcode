02bc1a4cd842c00645db54229c1a17e1b27b2c5a
feng <feng2860984180@163.com>
2023-10-08 18:39:37 +0800

    选题

---
+++ 160.相交链表.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=typescript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+
+};
+// @lc code=end
+
