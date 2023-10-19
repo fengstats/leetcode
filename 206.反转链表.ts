503872d8d4a136273dcb16f79fbf12c41d118592
feng <feng2860984180@163.com>
2023-10-20 02:58:11 +0800

    反转链表 | 迭代

--- 206.反转链表.ts
+++ 206.反转链表.ts
@@ -17,9 +17,31 @@
  * }
  */
 
+// 迭代：一次循环搞定
+function reverseList(head: ListNode | null): ListNode | null {
+  // 上一个的节点
+  let prev = null
+  // 剩余节点，用于循环
+  let remain = head
+
+  while (remain) {
+    // 存储原有链表后面的节点不被更改（循环需要）
+    const next = remain.next
+    // 反转链表（第一个节点进来会被赋值为 null，第二个节点进来会被赋值为第一个节点）
+    remain.next = prev
+    // 给下个循环节点赋值使用
+    prev = remain
+    // 继续循环剩余节点
+    remain = next
+  }
+
+  // 退出循环时链表反转结束，prev 肯定最后一个节点
+  return prev
+}
+
 // 暴力：先用数组存储所有节点，通过逆向循环遍历从最后一个节点开始
 // 将其 next 指向前一个数组元素节点，直到循环结束，取出最后一个节点返回
-function reverseList(head: ListNode | null): ListNode | null {
+function reverseList1(head: ListNode | null): ListNode | null {
   if (!head) return null
 
   // 存储
