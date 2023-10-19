37d81e9b8d9cac7061325225810e75c290cd6e77
feng <feng2860984180@163.com>
2023-10-19 23:41:43 +0800

    相交链表 | 哈希

--- 160.相交链表.ts
+++ 160.相交链表.ts
@@ -5,20 +5,35 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
 
+// Definition for singly-linked list.
+// class ListNode {
+//   val: number
+//   next: ListNode | null
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val
+//     this.next = next === undefined ? null : next
+//   }
+// }
+
+// 哈希：通过 hashMap 存储第一个链表的所有节点，遍历第二个链表对比 hashMap 内是否存在该节点
+// 存在，该节点为相交点
+// 不存在，不存在相交点
 function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+  const hashMap = new Map<ListNode, number>()
 
-};
+  while (headA) {
+    hashMap.set(headA, 1)
+    // 指向下一个节点，直至 null 退出 while
+    headA = headA.next
+  }
+
+  while (headB) {
+    // 是否存在
+    if (hashMap.has(headB)) return headB
+    headB = headB.next
+  }
+
+  return null
+}
 // @lc code=end
-
