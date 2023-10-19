9fa366d9babb00828a64afdc89f5058d1e13d7ab
feng <feng2860984180@163.com>
2023-10-20 00:31:02 +0800

    相交链表 | 暴力

--- 160.相交链表.ts
+++ 160.相交链表.ts
@@ -16,10 +16,28 @@
 //   }
 // }
 
+// 暴力：链表 headA 所有节点和链表 headB 中的所有节点做比较
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+  // 临时链表存储（虽然题目没说不能污染原链表数据，但想了想还是别污染好了）
+  let pA = headA
+  while (pA) {
+    // 在这里创建临时 headB 是因为后面还需要比较 headA 中的其他节点
+    // 每次都是一个全新的 headB
+    let pB = headB
+    while (pB) {
+      if (pA === pB) return pA
+      pB = pB.next
+    }
+    pA = pA.next
+  }
+
+  return null
+}
+
 // 哈希：通过 hashMap 存储第一个链表的所有节点，遍历第二个链表对比 hashMap 内是否存在该节点
 // 存在，该节点为相交点
 // 不存在，不存在相交点
-function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+function getIntersectionNode1(headA: ListNode | null, headB: ListNode | null): ListNode | null {
   const hashMap = new Map<ListNode, number>()
 
   while (headA) {
