f695a459a5b6c23bd390448353662f23d873abc4
feng <feng2860984180@163.com>
2023-10-20 19:58:41 +0800

    反转链表 | 递归

--- 206.反转链表.ts
+++ 206.反转链表.ts
@@ -17,8 +17,31 @@
  * }
  */
 
-// 迭代：一次循环搞定
+// 递归：精髓就是理解 head.next.next = head 这个操作
 function reverseList(head: ListNode | null): ListNode | null {
+  // 第一层的边界处理，链表为空
+  if (!head) return null
+
+  // 在递归内将最后一个节点返回（单独写是为了不和第一层判断混淆）
+  if (head.next === null) return head
+
+  // 递归开始（入栈），最终返回值就是上面的那个判断，也就是最后一个节点
+  const newHead = reverseList(head.next)
+
+  // 假设链表为 a->b->c->null，递归能到这肯定是 b 节点（倒数第二个）
+  // 经过这个操作后会变成 b->c->b，也就是将 c 节点的指向改成了 b，开始反转
+  head.next.next = head
+  // 但是 b->c，c->b 这样就会成为一个环，如何要把这个环打破？通过 head.next = null
+  // 就是将 b.next 指向 null，这样结果就变成了 c->b->null
+  // 等这一层递归结束，下一层轮到 a 节点，重复上述操作，将变成 b->a->null
+  head.next = null
+
+  // 递归结束，返回链表新头节点，反转完成！
+  return newHead
+}
+
+// 迭代：一次循环搞定（双指针或三指针？用了一个临时节点存储）
+function reverseList2(head: ListNode | null): ListNode | null {
   // 上一个的节点
   let prev = null
   // 剩余节点，用于循环
