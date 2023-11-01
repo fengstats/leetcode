f4a19a3e4912555bac68258a64269ea2ee602e34
feng <feng2860984180@163.com>
2023-11-01 21:37:40 +0800

    颜色分类 | 计数排序代码优化

--- 75.颜色分类.ts
+++ 75.颜色分类.ts
@@ -8,9 +8,26 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
+
+// 计数排序代码优化
+function sortColors(nums: number[]): void {
+  // 三种颜色对应 map 中的 key
+  const map = new Map<number, number[]>([
+    [0, []],
+    [1, []],
+    [2, []],
+  ])
+
+  for (let num of nums) map.get(num)?.push(num)
+
+  // 将 Map 内的数组合并之后扁平化处理，最后赋值
+  const colorArr = Array.from(map.values()).flat()
+  for (let i in nums) nums[i] = colorArr[i]
+}
+
 // 计数排序：因为数组中只包含 0 1 2 三种颜色，通过记录三种颜色出现次数
 // 最后将三种颜色根据顺序 + 出现次数填入到数组中
-function sortColors(nums: number[]): void {
+function sortColors1(nums: number[]): void {
   let red = 0
   let white = 0
   let blue = 0
@@ -45,5 +62,3 @@ function sortColors(nums: number[]): void {
   }
 }
 // @lc code=end
-
-console.log(sortColors([2, 0, 2, 1, 1, 0]))
