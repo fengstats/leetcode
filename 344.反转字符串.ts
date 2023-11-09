c9908dc6a86c74fe9c571bb71d4cf3ce6d9d5927
feng <feng2860984180@163.com>
2023-11-09 20:24:06 +0800

    反转字符串 | 双指针

--- 344.反转字符串.ts
+++ 344.反转字符串.ts
@@ -8,8 +8,17 @@
 /**
  Do not return anything, modify s in-place instead.
  */
-// api
+
+// 双指针：左右两个指针分别向中间靠拢同时一一交换值，直到两个指针重合，交换完毕
 function reverseString(s: string[]): void {
+  let left = 0
+  let right = s.length - 1
+  // 通过数组解构交换值
+  while (left < right) [s[left++], s[right--]] = [s[right], s[left]]
+}
+
+// api
+function reverseString1(s: string[]): void {
   s.reverse()
 }
 // @lc code=end
