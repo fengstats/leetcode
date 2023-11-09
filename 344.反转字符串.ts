4c59073d732e37dd466dd4ea81976dc721fb8a02
feng <feng2860984180@163.com>
2023-11-09 19:32:35 +0800

    反转字符串 | api

---
+++ 344.反转字符串.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=typescript
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify s in-place instead.
+ */
+// api
+function reverseString(s: string[]): void {
+  s.reverse()
+}
+// @lc code=end
