c448a02252f0df6ab0569ca922537db3432d8188
feng <feng2860984180@163.com>
2023-11-30 05:20:45 +0800

    设计链表 | 虚拟头节点（快被官方给的越界下标调试调傻了）

--- 707.设计链表.ts
+++ 707.设计链表.ts
@@ -15,7 +15,77 @@
 //   }
 // }
 
+// 优化：使用虚拟头节点的方式，统一操作，简化代码
 class MyLinkedList {
+  dummyHead: ListNode | null
+  size: number
+  constructor() {
+    this.dummyHead = new ListNode(999)
+    this.size = 0
+  }
+
+  // 返回 index 位置的节点值
+  get(index: number): number {
+    // NOTE: 官方定义了数据范围 0 <= index, val <= 1000
+    // if (index < 0 || index > this.size - 1) return -1
+    let cur = this.dummyHead?.next
+    while (index && cur) {
+      cur = cur.next
+      index--
+    }
+    return cur?.val ?? -1
+  }
+
+  // 插入头节点
+  addAtHead(val: number): void {
+    // 先连接后面的节点，再连接前面的
+    this.dummyHead!.next = new ListNode(val, this.dummyHead!.next)
+    this.size++
+  }
+
+  // 插入尾节点
+  addAtTail(val: number): void {
+    let cur = this.dummyHead
+    while (cur && cur.next) {
+      cur = cur.next
+    }
+    cur!.next = new ListNode(val)
+    this.size++
+  }
+
+  // 在 index 位置前插入一个节点
+  addAtIndex(index: number, val: number): void {
+    // NOTE: 边界处理，居然真的会给越界下标给我，无语
+    // 这里越界条件是大于长度，等于长度的时候相当于是插入尾部节点
+    if (index > this.size) return
+
+    let cur = this.dummyHead
+    while (index && cur) {
+      cur = cur.next
+      index--
+    }
+    // 因为是从 dummy 节点开始遍历，所以这个 cur 其实就是 index 的上一个节点
+    cur!.next = new ListNode(val, cur?.next)
+    this.size++
+  }
+
+  // 删除 index 位置节点
+  deleteAtIndex(index: number): void {
+    // NOTE: 边界处理，居然真的会给越界下标给我，无语
+    if (index >= this.size) return
+
+    let cur = this.dummyHead
+    while (index && cur) {
+      cur = cur.next
+      index--
+    }
+    // cur.next 就是要删除的节点
+    cur!.next = cur?.next?.next || null
+    this.size--
+  }
+}
+
+class MyLinkedList1 {
   size: number
   head: ListNode | null = null
   constructor() {
@@ -84,8 +154,10 @@ class MyLinkedList {
     }
 
     const prevHead = this.getHead(index - 1)
-    prevHead && (prevHead.next = prevHead?.next?.next || null)
-    this.size--
+    if (prevHead) {
+      prevHead.next = prevHead?.next?.next || null
+      this.size--
+    }
   }
 }
 
@@ -101,14 +173,15 @@ class MyLinkedList {
 // @lc code=end
 
 var obj = new MyLinkedList()
-obj.addAtHead(0)
-obj.addAtIndex(1, 1)
-obj.get(2)
-obj.addAtHead(4)
-obj.get(2)
-obj.addAtHead(4)
-obj.get(2)
+obj.addAtHead(1)
+obj.addAtTail(3)
+obj.addAtIndex(1, 2)
+obj.get(1)
+obj.deleteAtIndex(1)
+obj.get(1)
 obj.get(3)
-obj.addAtIndex(1, 6)
-obj.addAtTail(1)
-obj.addAtHead(0)
+obj.deleteAtIndex(3)
+obj.deleteAtIndex(0)
+obj.get(0)
+obj.deleteAtIndex(0)
+obj.get(0)
