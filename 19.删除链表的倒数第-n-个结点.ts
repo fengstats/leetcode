7da7d9c4efc2d52106e93423e59ffd7d741f158e
feng <feng2860984180@163.com>
2023-12-02 16:03:19 +0800

    删除链表的倒数第 N 个节点 | 只用快慢双指针

--- 19.删除链表的倒数第-n-个结点.ts
+++ 19.删除链表的倒数第-n-个结点.ts
@@ -26,10 +26,34 @@ class ListNode {
   }
 }
 
+// 只用快慢指针
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  if (!head) return head
+
+  let fast: ListNode | null = head
+  let slow: ListNode | null = head
+
+  while (n--) fast = fast!.next
+
+  while (fast && fast.next) {
+    fast = fast.next
+    slow = slow!.next
+  }
+
+  // 退出循环条件是 fast.next = null
+  // 所以如果快指针直接走没了，那需要删除的就是第一个节点
+  if (fast === null) return head.next
+
+  // 删除目标节点
+  slow!.next = slow!.next!.next
+
+  return head
+}
+
 // 快慢双指针 + 虚拟头节点（统一操作，减少特殊情况判断）
 // 先让快指针走 n 步，然后让快指针和慢指针一起走，直到快指针的 next 为空
 // 此时慢指针位置就是需要删除节点的前一个位置，那怎么删就不用我说了吧？
-function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+function removeNthFromEnd1(head: ListNode | null, n: number): ListNode | null {
   if (!head) return head
 
   const dummyNode = new ListNode(999, head)
@@ -38,8 +62,7 @@ function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
 
   // 快指针走
   while (n) {
-    // 变量范围：1 <= n <= 链表节点数量
-    // 因为定义了虚拟头节点，所以这里 fast.next 不可能为 null
+    // 变量范围：1 <= n <= 链表节点数量，所以这里 fast.next 不可能为 null
     fast = fast!.next
     n--
   }
@@ -52,6 +75,8 @@ function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
   }
 
   // 删除倒数 n 个节点
+  // 这里就是虚拟头节点的好处了，不需要特殊判断
+  // 如果要删的是第一个，那么 slow 就是 dummyNode
   slow!.next = slow!.next!.next
 
   return dummyNode.next
