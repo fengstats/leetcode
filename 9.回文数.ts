be417eda649f2c355f3058aa4ed29d411f0b3463
fengstats <feng2860984180@163.com>
2023-12-16 20:11:05 +0800

    回文数 | 双指针

--- 9.回文数.ts
+++ 9.回文数.ts
@@ -5,8 +5,25 @@
  */
 
 // @lc code=start
-// 暴力：转换成字符串、转列表、再反转，最后转回数字，通过对比正序倒序数字判断是否为回文数
+
+// 双指针
 function isPalindrome(x: number): boolean {
+  const arr = Array.from(String(x))
+  let left = 0
+  let right = arr.length - 1
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
+      return false
+    }
+    left++
+    right--
+  }
+
+  return true
+}
+
+// 暴力：转换成字符串、转列表、再反转，最后转回数字，通过对比正序倒序数字判断是否为回文数
+function isPalindrome1(x: number): boolean {
   const y = Number((x + '').split('').reverse().join(''))
   if (x === y) return true
   else return false
