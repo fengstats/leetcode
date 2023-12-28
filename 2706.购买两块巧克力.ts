7b3d591eec0e9eedd3ec1047fe5f092417385a28
fengstats <feng2860984180@163.com>
2023-12-29 03:30:42 +0800

    购买两块巧克力 | 一次遍历

--- 2706.购买两块巧克力.ts
+++ 2706.购买两块巧克力.ts
@@ -6,8 +6,32 @@
 
 // @lc code=start
 
-// 排序
+// 一次遍历
 function buyChoco(prices: number[], money: number): number {
+  // 维护两个变量，一个是最低价格、一个是次最低价格
+  let minPrice = Math.min(prices[0], prices[1])
+  let nextMinPrice = Math.max(prices[0], prices[1])
+
+  for (let i = 2; i < prices.length; i++) {
+    if (minPrice > prices[i]) {
+      // 优先比较最小值更新
+      // 比最小值小，那么原来的最小值就是次之了
+      nextMinPrice = minPrice
+      minPrice = prices[i]
+    } else if (nextMinPrice > prices[i]) {
+      // 只比次最小值小，直接更新即可
+      nextMinPrice = prices[i]
+    }
+  }
+
+  // 计算剩余资金
+  const residualMoney = money - (minPrice + nextMinPrice)
+
+  return residualMoney >= 0 ? residualMoney : money
+}
+
+// 排序
+function buyChoco1(prices: number[], money: number): number {
   // 数组原地升序排列（从小到大）
   prices.sort((a, b) => a - b)
 
