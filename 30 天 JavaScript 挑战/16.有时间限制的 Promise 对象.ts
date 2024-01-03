b966e5cf5e91a56044f2e53bf20975c8b2433cc8
fengstats <feng2860984180@163.com>
2024-01-03 21:35:44 +0800

    有时间限制的 Promise 对象 | Promise.race

--- 30 天 JavaScript 挑战/16.有时间限制的 Promise 对象.ts
+++ 30 天 JavaScript 挑战/16.有时间限制的 Promise 对象.ts
@@ -1,14 +1,25 @@
 type Fn = (...params: any[]) => Promise<any>
 
-// 内部一个定时器执行规定时间，规定时间内如果 Promise.resolve 没有返回
-// 那就返回 Promise.reject
+// Promise.race 接收一个 promise 数组，谁快谁先返回
 function timeLimit(fn: Fn, t: number): Fn {
   return async function (...args) {
+    const error = new Promise((_, reject) => setTimeout(reject, t, 'Time Limit Exceeded'))
+    // fn 函数本身就返回一个 promise 对象
+    return Promise.race([error, fn(...args)])
+  }
+}
+
+// 内部一个定时器执行规定时间，规定时间内如果 Promise.resolve 没有返回
+// 那就返回 Promise.reject
+function timeLimit1(fn: Fn, t: number): Fn {
+  return async function (...args) {
     return new Promise((resolve, reject) => {
-      setTimeout(reject, t, 'Time Limit Exceeded')
+      const timeoutId = setTimeout(reject, t, 'Time Limit Exceeded')
       fn(...args)
         .then(resolve)
         .catch(reject)
+        // NOTE: 良好习惯，清理定时器
+        .finally(() => clearInterval(timeoutId))
     })
   }
 }
