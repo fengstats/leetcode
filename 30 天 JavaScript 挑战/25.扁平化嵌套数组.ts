b2ce5a8fda9ded6856092bc8ac09bf216a304f8d
fengstats <feng2860984180@163.com>
2024-01-12 21:10:22 +0800

    扁平化嵌套数组 | 迭代 + 递归优化

--- 30 天 JavaScript 挑战/25.扁平化嵌套数组.ts
+++ 30 天 JavaScript 挑战/25.扁平化嵌套数组.ts
@@ -1,7 +1,41 @@
 type MultiDimensionalArray = (number | MultiDimensionalArray)[]
 
-// 递归
+// 递归优化
 var flat = function (arr: MultiDimensionalArray, n: number): MultiDimensionalArray {
+  // 这样 ans 就只会被声明一次啦
+  const ans: MultiDimensionalArray = []
+
+  // 内部处理，展开完了就 push 给 ans
+  function flattening(newArr, expandNum) {
+    for (const item of newArr) {
+      // NOTE: 这里判断写成 n > 0 了，给我找半天！我说怎么内部元素全给展开了
+      if (expandNum > 0 && Array.isArray(item)) {
+        flattening(item, expandNum - 1)
+      } else {
+        ans.push(item)
+      }
+    }
+  }
+
+  // 开始递归
+  flattening(arr, n)
+  // 返回结果
+  return ans
+}
+
+// 迭代
+var flat2 = function (arr: MultiDimensionalArray, n: number): MultiDimensionalArray {
+  // 是否需要展开，且判断数组中是否还有没展开的数组元素
+  while (n > 0 && arr.some(Array.isArray)) {
+    // 一次展开所有数组内部元素一层，通过结合 ... 和 concat
+    arr = [].concat(...(arr as []))
+    n--
+  }
+  return arr
+}
+
+// 递归
+var flat1 = function (arr: MultiDimensionalArray, n: number): MultiDimensionalArray {
   // 展开不了了
   if (n <= 0) return arr
 
@@ -22,4 +56,5 @@ var flat = function (arr: MultiDimensionalArray, n: number): MultiDimensionalArr
   return ans
 }
 
-console.log(flat([1, [2, 3, [4, 5]]], 2))
+// console.log(flat([1, [2, 3, [4, 5]]], 2))
+console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 1))
