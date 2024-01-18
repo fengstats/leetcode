eb3b354cc84ad619c85b3aee3fd51d892e163ebe
fengstats <feng2860984180@163.com>
2024-01-19 00:44:47 +0800

    统计出现过一次的公共字符串 | 单个 hashMap

--- 2085.统计出现过一次的公共字符串.ts
+++ 2085.统计出现过一次的公共字符串.ts
@@ -6,8 +6,41 @@
 
 // @lc code=start
 
-// hashMap
+// 单个 hashMap
 function countWords(words1: string[], words2: string[]): number {
+  let ans = 0
+  const map = new Map()
+
+  // 初始化
+  for (const word of words1) {
+    if (!map.has(word)) {
+      // 只出现一次
+      map.set(word, 1)
+    } else {
+      // NOTE: 这里用无穷大表示这个 word 已经出现多次，后续不能计入答案了
+      map.set(word, Infinity)
+    }
+  }
+
+  for (const word of words2) {
+    // NOTE: 只增加 map 中已存在的值的出现次数
+    if (map.has(word)) {
+      map.set(word, map.get(word) + 1)
+    }
+  }
+
+  // 把最后加起来等于 2 的值出现次数提取出来统计
+  for (const count of map.values()) {
+    if (count === 2) {
+      ans++
+    }
+  }
+
+  return ans
+}
+
+// hashMap
+function countWords1(words1: string[], words2: string[]): number {
   const map1 = new Map()
   const map2 = new Map()
 
